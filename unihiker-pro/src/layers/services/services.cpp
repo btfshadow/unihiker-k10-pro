3534c0892698eeb9b649e2a0985426af41d2c9a7
Sophia Simoes <sophia.simoesg@gmail.com>
2026-05-04 01:56:57 -0300

    all resolutions on services

--- unihiker-pro/src/layers/services/services.cpp
+++ unihiker-pro/src/layers/services/services.cpp
@@ -20,6 +20,8 @@ struct LiveModeItem {
 };
 
 static const LiveModeItem kLiveModes[] = {
+  {FRAMESIZE_QVGA, "QVGA 320x240"},
+  {FRAMESIZE_VGA, "VGA 640x480"},
   {FRAMESIZE_SVGA, "SVGA 800x600"},
   {FRAMESIZE_XGA, "XGA 1024x768"},
   {FRAMESIZE_SXGA, "SXGA 1280x1024"},
